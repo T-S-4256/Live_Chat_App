3373140161e658b91e76f2ac4a6e48ee0889abdc
T-S-4256 <sp4256824@gmail.com>
2025-06-11 23:58:31 +0530

    Some Changes

--- Client/src/pages/HomePage.jsx
+++ Client/src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState } from 'react'
-import Sidebar from '../components/Sidebar'
-import ChatContainer from '../components/ChatContainer'
-import RightSideBar from '../components/RightSideBar'
-import { ChatContext } from '../../context/ChatContext'
+import Sidebar from '../components/Sidebar.jsx'
+import ChatContainer from '../components/ChatContainer.jsx'
+import RightSideBar from "../components/RightSideBar.jsx"
+import { ChatContext } from '../../context/ChatContext.jsx'
 
 const HomePage = () => {
   const {selectedUser}=useContext(ChatContext);
